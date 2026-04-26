1298f5eb80bff7901a071397a3fa97272e5fe412
Êmile <emileferreira.dev@gmail.com>
2026-04-26 19:25:25 -0300

    fix: corrige import generatePDF

--- src/pages/Fornecedores.tsx
+++ src/pages/Fornecedores.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { loadSuppliers, deleteSupplier } from '../lib/supabaseHelpers'
 import { Supplier } from '../types'
-import { generatePDF } from '../lib/GeneratePDF'
+import { generatePDF } from '../lib/generatePDF'
 
 function StatusBadge({ status }: { status?: string }) {
     const map: Record<string, string> = {
